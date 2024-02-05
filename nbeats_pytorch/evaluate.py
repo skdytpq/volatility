87760ddbb924b32078b0ed60e6e26359fe531490
joseph <skdytpq98@naver.com>
2024-02-05 23:03:18 +0900

    f

--- nbeats_pytorch/evaluate.py
+++ nbeats_pytorch/evaluate.py
@@ -59,7 +59,7 @@ def evaluate(model, loss_fn, test_loader, params, plot_num, sample=True):
           batch_size = test_batch.shape[1]
           test_batch = test_batch.to(torch.float32).to(params.device)  # not scaled
          # labels_batch = labels_batch.to(torch.float32).to(params.device)  # not scaled
-          labels_batch = labels[:,-params.forecast_length:] # Batch X 1 # backcast 지정
+          labels_batch = labels # Batch X 1 # backcast 지정
           test_batch = test_batch[:,:-params.forecast_length,:].squeeze(-1)# 23 , batch , 1 backcast = 1
           #labels_batch = test_batch.unsqueeze(-1)[:,:,-1]
       #    idx = idx.unsqueeze(0).to(params.device)
